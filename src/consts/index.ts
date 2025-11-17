d3ff4a7a5a8f9f8feeb1fa099925ed684942f941
Ameer12348 <ameerarif12348@gmail.com>
2025-11-17 12:51:42 +0500

    fixed the issue of auth

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,4 +1,4 @@
 export const BASE_URL = process.env.API_BASE_URL || 'https://scoopnation.pk/api_admin/public';
 export const IMAGE_BASE_URL = process.env.IMAGE_BASE_URL || 'https://scoopnation.pk/api_admin/public';
-// export const BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080';
-// export const IMAGE_BASE_URL = process.env.IMAGE_BASE_URL || 'http://localhost:8080';
+// export const BASE_URL = process.env.API_BASE_URL || 'http://localhost:8081';
+// export const IMAGE_BASE_URL = process.env.IMAGE_BASE_URL || 'http://localhost:8081';
